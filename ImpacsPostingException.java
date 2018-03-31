81beb49a39b95ef539e4f57b66c0a4d71f26aab2
usanker <usanker@gmail.com>
2018-03-31 17:07:31 -0500

    ImpacsPostingException__JAVA
    ImpacsPostingException__JAVA

---
+++ ImpacsPostingException.java
@@ -0,0 +1,22 @@
+
+public class ImpacsPostingException extends Exception{
+	
+    static final long serialVersionUID = 1L; 
+	
+	public ImpacsPostingException(String message) {
+		super(message);
+	}
+	
+	public ImpacsPostingException(Throwable cause) {
+		super(cause);
+	}
+	
+	public ImpacsPostingException(String message, Throwable cause) {
+		super(message,cause);
+	}
+	
+	public String toString(){ 
+		return ("") ;
+	}
+
+}
